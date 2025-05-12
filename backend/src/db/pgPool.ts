29d7547c101057b346ade62721e989842d20409a
Marius Iordache <marius.technical.leader@gmail.com>
2025-05-12 13:16:49 -0400

    fixed db config dev + prod

--- backend/src/db/pgPool.ts
+++ backend/src/db/pgPool.ts
@@ -1,6 +1,16 @@
-import { Pool } from "pg";
+import { Pool } from "pg"
+import { config } from "dotenv"
+import path from "path"
+
+
+// we need to load the .env file for non-production environments
+if (process.env.NODE_ENV !== "skip-env-loading") {
+    const result = config({
+        path: path.resolve(__dirname, "../../.env"), // Look for .env one level up from /src
+    })
+}
 
 export const pgPool = new Pool({
     connectionString: process.env.DATABASE_URL,
     max: 10
-});
+})
